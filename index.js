fdf06f9f834e766f26961ab4dc790a286c6b1722
Sammy Saglam <sami@saglam.tk>
2018-03-31 16:49:39 +0200

    fixed: load react's dev prop-types instead of the prod empty one

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-var PropTypes = require('../prop-types');
+// ------------------------------------------------
+// load original React prop-types
+// ------------------------------------------------
+var REACT_ELEMENT_TYPE = (typeof Symbol === 'function' &&
+	Symbol.for &&
+	Symbol.for('react.element')) ||
+	0xeac7;
+
+var isValidElement = function(object) {
+	return typeof object === 'object' &&
+		object !== null &&
+		object.$$typeof === REACT_ELEMENT_TYPE;
+};
+var throwOnDirectAccess = true;
+var PropTypes = require('../prop-types/factoryWithTypeCheckers')(isValidElement, throwOnDirectAccess);
+// ------------------------------------------------
+// ------------------------------------------------
+// ------------------------------------------------
 
 var AxePropTypes = {};
 
